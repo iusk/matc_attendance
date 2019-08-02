9d2a76847aa5d0c0ea1083e04e7e95243e658762
Aayush Khatiwada <mystaraaush@gmail.com>
2019-08-02 09:16:03 -0500

    Implementing Authentication system

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, AsyncStorage, Button } from 'react-native'
 import styles from './styles';
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'Home'
     };
+
+    state = { 
+        userId: 0
+    }
+
+    _getUserId = async () => {
+        const userId = await AsyncStorage.getItem('userId');
+        console.log(userId);
+        this.setState( {userId: userId} );
+    }
+
+    signOut = async () => {
+        await AsyncStorage.clear();
+        console.log('cleared');
+        this.props.navigation.navigate('Auth');
+    }
+
     render() {
+        this._getUserId;
         return (
-            <View style={styles.mainWrapper}>
-                <View style={styles.contentWrapper}>
-                    <Text>Mentor Id: {this.props.navigation.getParam('mentorId')}</Text>
-                </View>
-                <View style={styles.footerWrapper}>
-                    <View></View>
-                </View>
+            <View style={styles.contentWrapper}>
+            <Text>Mentor Id: {this.state.userId}</Text>
+                <Button onPress={this.signOut} title="Sign Out" />
+                <Button title="userid" onPress={this._getUserId} />
             </View>
         );
     }
